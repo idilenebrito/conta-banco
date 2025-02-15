9814fb141125347fa5bae48639ea6f3fa171d197
Idilene Brito <idilenebrito98.ib@gmail.com>
2025-02-14 23:02:45 -0300

    feat: add conta terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta!");
+        String numero = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = sc.nextLine();
+        
+        System.out.println("Por favor, digite o nome do cliente!");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta!");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível  para saque");
+
+        sc.close();
+    }
+}
